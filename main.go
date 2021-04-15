224c01dce20c125b593e2c7898360566558c1041
Vincent Duong <vincent.duong.dev@gmail.com>
2021-04-15 19:40:13 +0200

    Parse file lines

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func getLinesFromFile(filePath string) []string {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var (
+		lines    []string
+		bytes    []byte
+		isPrefix bool = false
+	)
+
+	reader := bufio.NewReader(file)
+	for !isPrefix && err == nil {
+		bytes, isPrefix, err = reader.ReadLine()
+		lines = append(lines, string(bytes))
+	}
+
+	return lines
+}
+
+func main() {
+	fileName := os.Args[1]
+
+	lines := getLinesFromFile(fileName)
+	fmt.Println("'" + strings.Join(lines, `','`) + `'`)
+}
