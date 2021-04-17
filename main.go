044123469de2098fc5ad49a408cdaf085f7a8597
Vincent Duong <vincent.duong.dev@gmail.com>
2021-04-17 10:07:14 +0200

    Add getPuzzleFromLines function

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -30,9 +31,63 @@ func getLinesFromFile(filePath string) []string {
 	return lines
 }
 
+func getPuzzleFromLines(lines []string) [][]int {
+	var (
+		err        error
+		currentRow int = 0
+		puzzleSize int = -1
+		puzzle     [][]int
+	)
+
+	for _, line := range lines {
+		fmt.Printf("\nLine: \"%v\"\n", line)
+		if len(line) == 0 || line[0] == '#' {
+			fmt.Println("Comment")
+			continue
+		}
+
+		if puzzleSize == -1 {
+			puzzleSize, err = strconv.Atoi(line)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("Puzzle size: %v\n", puzzleSize)
+
+			puzzle = make([][]int, puzzleSize)
+			for i := range puzzle {
+				puzzle[i] = make([]int, puzzleSize)
+			}
+
+			continue
+		}
+
+		fmt.Printf("Current Row: %d\n", currentRow)
+		numbers := strings.Split(line, " ")
+		for i, stringNumber := range numbers {
+
+			if i == puzzleSize {
+				log.Fatal("File format is not valid")
+			}
+
+			number, err := strconv.Atoi(stringNumber)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			puzzle[currentRow][i] = number
+		}
+		currentRow++
+	}
+
+	return puzzle
+}
+
 func main() {
 	fileName := os.Args[1]
 
 	lines := getLinesFromFile(fileName)
 	fmt.Println("'" + strings.Join(lines, `','`) + `'`)
+	puzzle := getPuzzleFromLines(lines)
+	fmt.Println(puzzle)
 }
