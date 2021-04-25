44e5caa17a63abce28a95727f166e6b6697f8d10
Vincent Duong <vincent.duong.dev@gmail.com>
2021-04-25 09:07:37 +0200

    A star works for 3x3 puzzle

--- src/main.go
+++ src/main.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strings"
 
 	"github.com/vinceduong/n-puzzle/src/parse"
 )
 
 type Node struct {
-	puzzle          [][]int
-	cost, heuristic int
-	zeroPosition    Position
-	parent          *Node
+	puzzle                 [][]int
+	cost, heuristic, score int
+	zeroPosition           Position
+	parent                 *Node
 }
 
 type Position struct {
@@ -100,12 +101,20 @@ func ManhattanDistance(row int, column int, pos Position) int {
 	return Abs(row-pos.row) + Abs(column-pos.column)
 }
 
+func isMissPlaced(row int, column int, pos Position) int {
+	if row != pos.row || column != pos.column {
+		return 1
+	}
+	return 0
+}
+
 func Heuristic(puzzle [][]int, solvedPiecePositions map[int]Position) int {
 	heuristic := 0
 
 	for i := range puzzle {
 		for j, pieceNumber := range puzzle[i] {
-			distance := ManhattanDistance(i, j, solvedPiecePositions[pieceNumber])
+			// distance := ManhattanDistance(i, j, solvedPiecePositions[pieceNumber])
+			distance := isMissPlaced(i, j, solvedPiecePositions[pieceNumber])
 			heuristic += distance
 		}
 	}
@@ -206,17 +215,51 @@ func Neighbors(node *Node, solvedPiecePositions map[int]Position) []Node {
 	for i, position := range potentialZeroPositions {
 		newPuzzle := SwapPuzzlePieces(node.puzzle, node.zeroPosition, position)
 		cost := node.cost + 1
-		heuristic := Heuristic(newPuzzle, solvedPiecePositions) + cost
+		heuristic := Heuristic(newPuzzle, solvedPiecePositions)
+		score := cost + heuristic
 
 		neighbors[i] = Node{
 			newPuzzle,
-			cost, heuristic,
+			cost, heuristic, score,
 			position,
 			node,
 		}
 	}
 
 	return neighbors
+}
+
+func isSame(puzzle1 [][]int, puzzle2 [][]int) bool {
+	for i := range puzzle1 {
+		for j := range puzzle1[i] {
+			if puzzle1[i][j] != puzzle2[i][j] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func nodeIsWorth(closedList []Node, openList []Node, node Node) bool {
+	// fmt.Println("----------CHILD NODE---------")
+	// prettyNode(node)
+	for _, n := range closedList {
+		if isSame(n.puzzle, node.puzzle) {
+			// fmt.Print("Node is not worth\n")
+			return false
+		}
+	}
+
+	for _, n := range openList {
+		if isSame(n.puzzle, node.puzzle) && n.cost < node.cost {
+			// fmt.Print("Node is not worth\n")
+			return false
+		}
+	}
+
+	// fmt.Print("Node is worth\n")
+	return true
 }
 
 func main() {
@@ -229,11 +272,12 @@ func main() {
 	fmt.Println("'" + strings.Join(lines, `','`) + `'`)
 	puzzle, puzzleSize := parse.PuzzleFromLines(lines)
 	zeroPosition := ZeroPosition(puzzle)
-
 	fmt.Printf("Puzzle: %v\n\n", puzzle)
 	fmt.Printf("Zero position: %v\n\n", zeroPosition)
 
 	solvedPuzzle, piecePositions := SolvedPuzzle(puzzleSize)
+	fmt.Printf("Is solved: %v\n", isSame(puzzle, solvedPuzzle))
+
 	fmt.Printf("Solved Puzzle: %v\n\n", solvedPuzzle)
 	fmt.Printf("Heuristic: %v\n\n", Heuristic(solvedPuzzle, piecePositions))
 	fmt.Printf("New Puzzle: %v\n", PotentialZeroPositions(zeroPosition, puzzleSize))
@@ -243,12 +287,51 @@ func main() {
 
 	rootNode := Node{
 		puzzle,
-		0, Heuristic(puzzle, piecePositions),
+		0, Heuristic(puzzle, piecePositions), Heuristic(puzzle, piecePositions),
 		zeroPosition,
 		nil,
 	}
 
-	prettyNode(rootNode)
+	// prettyNode(rootNode)
 
-	prettyNodes(Neighbors(&rootNode, piecePositions))
+	// prettyNodes(Neighbors(&rootNode, piecePositions))
+
+	closedList := make([]Node, 0)
+	openList := make([]Node, 1)
+	openList[0] = rootNode
+	var node Node
+
+	for len(openList) > 0 {
+		// time.Sleep(1000 * time.Millisecond)
+
+		node, openList = openList[0], openList[1:]
+		// prettyNode(node)
+		fmt.Printf("Len: %v\n", len(openList))
+
+		if isSame(node.puzzle, solvedPuzzle) {
+			fmt.Println("Puzzle is solved")
+
+			prettyNode(node)
+			return
+		}
+
+		for _, neighbor := range Neighbors(&node, piecePositions) {
+			if nodeIsWorth(closedList, openList, neighbor) {
+				openList = append(openList, neighbor)
+			}
+		}
+
+		sort.Slice(openList, func(i, j int) bool {
+			if openList[i].heuristic == openList[j].heuristic {
+				return openList[i].score < openList[j].score
+			} else {
+				return openList[i].heuristic < openList[j].heuristic
+			}
+		})
+
+		for _, node := range openList {
+			fmt.Printf("Heuristic: %v, cost: %v\n", node.heuristic, node.cost)
+		}
+		closedList = append(closedList, node)
+	}
 }
